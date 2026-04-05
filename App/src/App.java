fee89d6dbeae6b722b483d6afafb2b8f0fa87fbd
hazelemma703 <hazelemma703@gmail.com>
2026-04-05 12:37:20 +0000

    fis> >(

---
+++ App/src/App.java
@@ -0,0 +1,35 @@
+import java.util.List;
+
+public class App {
+    public static void main(String[] args) throws Exception {
+        SistemaRecomendacion sistema = new SistemaRecomendacion();
+
+        // Crear Courses
+        Course c1 = new Course("Programación en Java");
+        c1.agregarTema("programacion");
+
+        Course c2 = new Course("Introducción a IA");
+        c2.agregarTema("ia");
+
+        Course c3 = new Course("Bases de Datos");
+        c3.agregarTema("datos");
+
+        sistema.agregarCourse(c1);
+        sistema.agregarCourse(c2);
+        sistema.agregarCourse(c3);
+
+        // Crear Student
+        Student est = new Student("Kevin");
+        est.agregarInteres("programacion");
+        est.agregarInteres("ia");
+
+        // Recomendación
+        List<Course> recomendados = sistema.recomendar(est);
+
+        System.out.println("Courses recomendados:");
+
+        for (Course c : recomendados) {
+            System.out.println("- " + c.nombre);
+        }
+    }
+}
